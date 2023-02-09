0f675e06d668d1551cc9b005c2a60bdb7afd183e
Albim360 <alberto.zappala@icloud.com>
2023-02-09 14:11:39 +0100

    FEAT aggiunta griglia e funzione on click con id num

---
+++ js/app.js
@@ -0,0 +1,33 @@
+console.log('prato fiorito');
+
+// Dichiara la variabile per il lato della griglia
+let latoGriglia = 10;
+
+// Calcola il numero totale di celle nella griglia
+let numeroCelle = latoGriglia * latoGriglia;
+
+// Seleziona l'elemento con classe "griglia" dalla pagina HTML
+const grigliaElement = document.querySelector('.griglia');
+
+// Crea un ciclo per generare tutte le celle della griglia
+for(let i = 0; i < numeroCelle; i++) {
+    let num = i + 1;
+    
+    // Crea una stringa HTML per ogni cella
+    let divString = "<div class='cella' id='cella-" + num + "' style='width: calc(100% / " + latoGriglia + ")'>" + num + "</div>";
+    console.log(divString);
+
+    // Aggiunge la stringa HTML all'elemento della griglia
+    grigliaElement.innerHTML += divString;
+}
+
+// Seleziona tutti gli elementi con classe "cella" dalla pagina HTML
+const cella = document.querySelectorAll('.cella');
+
+// Itera su ogni cella e associa l'evento "click"
+cella.forEach(function(cella) {
+    cella.addEventListener('click', function() {
+        // Logga l'ID della cella cliccata
+        console.log(cella.id.split("-")[1]);
+    });
+});
