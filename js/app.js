0f918ff45b4c3f669e9e985a3167c121b89e4768
Albim360 <alberto.zappala@icloud.com>
2023-02-09 14:44:22 +0100

    FEAT aggiunti commenti e formattato documenti

--- js/app.js
+++ js/app.js
@@ -1,45 +1,43 @@
 console.log('prato fiorito');
 
-
-// Dichiara la variabile per il lato della griglia
+// Dichiariamo la variabile per il lato della griglia
 let latoGriglia = 10;
 
-// Calcola il numero totale di celle nella griglia
+// Calcoliamo il numero totale di celle nella griglia
 let numeroCelle = latoGriglia * latoGriglia;
 
+// Selezioniamo il pulsante "startGame" dalla pagina HTML
+const startButton = document.querySelector('#startGame');
 
-const startButton = document.querySelector('#startGame')
-
-
-
-
-// Seleziona l'elemento con classe "griglia" dalla pagina HTML
+// Selezioniamo l'elemento con classe "griglia" dalla pagina HTML
 const grigliaElement = document.querySelector('.griglia');
-startButton.addEventListener('click', function() {
-    document.querySelector('#startGame').remove()
 
-    // Crea un ciclo per generare tutte le celle della griglia
-    for(let i = 0; i < numeroCelle; i++) {
-      let num = i + 1;
-      
-      // Crea una stringa HTML per ogni cella
-      let divString = "<div class='cella' id='cella-" + num + "' style='width: calc(100% / " + latoGriglia + ")'>" + num + "</div>";
-      console.log(divString);
-  
-      // Aggiunge la stringa HTML all'elemento della griglia
-      grigliaElement.innerHTML += divString;
-    }
-    
-    // Seleziona tutti gli elementi con classe "cella" dalla pagina HTML
-    const cella = document.querySelectorAll('.cella');
-  
-    // Itera su ogni cella e associa l'evento "click"
-    cella.forEach(function(cella) {
-      cella.addEventListener('click', function() {
-        cella.classList.add('bg-brown');
-        // Logga l'ID della cella cliccata
-        console.log(cella.id.split("-")[1]);
-      });
+// Aggiungiamo un evento al clic sul pulsante "startGame"
+startButton.addEventListener('click', function() {
+  // Rimuoviamo il pulsante "startGame" dalla pagina
+  document.querySelector('#startGame').remove();
+
+  // Creiamo un ciclo per generare tutte le celle della griglia
+  for(let i = 0; i < numeroCelle; i++) {
+    let num = i + 1;
+
+    // Creiamo una stringa HTML per ogni cella
+    let divString = "<div class='cella' id='cella-" + num + "' style='width: calc(100% / " + latoGriglia + ")'>" + num + "</div>";
+    console.log(divString);
+
+    // Aggiungiamo la stringa HTML all'elemento della griglia
+    grigliaElement.innerHTML += divString;
+  }
+
+  // Selezioniamo tutti gli elementi con classe "cella" dalla pagina HTML
+  const cella = document.querySelectorAll('.cella');
+
+  // Iteriamo su ogni cella e associamo l'evento "click"
+  cella.forEach(function(cella) {
+    cella.addEventListener('click', function() {
+      cella.classList.add('bg-brown');
+      // Logghiamo l'ID della cella cliccata
+      console.log(cella.id.split("-")[1]);
     });
   });
-  
+});
