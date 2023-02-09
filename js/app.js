79bf1321a405f48cfd65fa3ddc6a2338cbc06d98
Alberto <74522184+albim360@users.noreply.github.com>
2023-02-09 18:30:47 +0100

    FEAT aggiunte animazioni, cambiati colori + restar

--- js/app.js
+++ js/app.js
@@ -12,13 +12,22 @@ const startButton = document.querySelector('#startGame');
 // Selezioniamo l'elemento con classe "griglia" dalla pagina HTML
 const grigliaElement = document.querySelector('.griglia');
 
+// Selezioniamo il pulsante "restartGame" dalla pagina HTML
+const restartButton = document.querySelector('#restartGame');
+
+// Nascondiamo il pulsante "restartGame" all'inizio
+restartButton.style.display = 'none';
+
 // Aggiungiamo un evento al clic sul pulsante "startGame"
-startButton.addEventListener('click', function() {
+startButton.addEventListener('click', function () {
   // Rimuoviamo il pulsante "startGame" dalla pagina
   document.querySelector('#startGame').remove();
 
+  // Mostriamo il pulsante "restartGame"
+  restartButton.style.display = 'block';
+
   // Creiamo un ciclo per generare tutte le celle della griglia
-  for(let i = 0; i < numeroCelle; i++) {
+  for (let i = 0; i < numeroCelle; i++) {
     let num = i + 1;
 
     // Creiamo una stringa HTML per ogni cella
@@ -33,11 +42,29 @@ startButton.addEventListener('click', function() {
   const cella = document.querySelectorAll('.cella');
 
   // Iteriamo su ogni cella e associamo l'evento "click"
-  cella.forEach(function(cella) {
-    cella.addEventListener('click', function() {
+  cella.forEach(function (cella) {
+    cella.addEventListener('click', function () {
       cella.classList.add('bg-green');
       // Logghiamo l'ID della cella cliccata
       console.log(cella.id.split("-")[1]);
+    });
+  });
+});
+
+// Aggiungiamo un evento al clic sul pulsante "restartGame"
+restartButton.addEventListener('click', function () {
+  grigliaElement.innerHTML = "";
+
+  for (let i = 0; i < numeroCelle; i++) {
+    let num = i + 1;
+    let divString = "<div class='cella' id='cella-" + num + "' style='width: calc(100% / " + latoGriglia + ")'>" + num + "</div>";
+    grigliaElement.innerHTML += divString;
+  }
+
+  const cella = document.querySelectorAll('.cella');
+  cella.forEach(function (cella) {
+    cella.addEventListener('click', function () {
+      cella.classList.add('bg-green');
     });
   });
 });
